d1d343842a79810dbe26fd424d18fe792e4ccbab
oderathegreat <oderathegreat@gmail.com>
2022-08-16 18:08:27 +0300

    added unordered list for messages display

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useEffect } from 'react';
+import useLocalStorage from 'react-use-localstorage';
+
+import { useImmer } from 'use-immer';
+import { useOnlineStatus } from '@withvoid/melting-pot';
+
 
 function App() {
+
+  const [data, setData] = useLocalStorage('storage_id', default_value);
+  const [room, setRoom] = useLocalStorage('room','');
+ const [id, setId] = useLocalStorage('id','');
+
+//check if user is online
+
+
+const [onlineList, setOnline] = useImmer([]);
+const { online } = useOnlineStatus();
+
+
+
   return (
+
+    
     <div className="App">
  
- 
+ <ul id="messages"><Messages data={messages} /></ul>
+<ul id="online"> {online ? '❤️ You are Online' : '💛 You are Offline'} <hr/><Online data={onlineList} /> </ul>
+
+
+
     </div>
   );
 }
